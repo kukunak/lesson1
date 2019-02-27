26a689a5ab1bb296b1abcba9fb90540e791848d5
Yan Pelyukhov <ypel@ya.ru>
2019-02-27 21:03:05 +0300

    First version

---
+++ home1/info_h.py
@@ -0,0 +1,5 @@
+user_info = {
+    "first_name": "Yan", 
+    "last_name": "Pelyukhov"
+}
+print (user_info["first_name"])
---
+++ home1/types_h.py
@@ -0,0 +1,19 @@
+a = 2
+b = 0.5
+print (a + b)
+
+name = 'Yan'
+print (f'Привет, {name}!')
+
+v = input ('Введите число от 1 до 10: ')
+print (int(v) + 10)
+
+name = input ('Введите ваше имя: ')
+print (f'Привет, {name}! Как дела?')
+
+print (float('1'))
+print (int(float('2.5')))
+print (bool(1))
+print (bool(''))
+print (bool(0))
+print ('2.5')
